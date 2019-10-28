d1afdb0cf862368817354626b727547afe722eb3
JoeCodeswell <joecodeswell@gmail.com>
2019-10-28 13:21:15 -0700

    PRogress - NO CIGAR 1 file modified, 1 added  see notes in card-joe.component.html

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import {
   MdToolbarModule,
   MdTabsModule,
@@ -10,14 +12,20 @@ import {
   MdDatepickerModule,
   MdNativeDateModule,
   MdCheckboxModule,
-  MdRadioModule
+  MdRadioModule,
+  MatCardModule
+
+  // MdCard
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CardJoeComponent } from './card-joe/card-joe.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule, 
+  MatCardModule
+  // MdCard
+  ],
   declarations: [ AppComponent, CardJoeComponent ],
   bootstrap:    [ AppComponent ]
 })
