d2ef7e6baff11479bc2d785216d9be706ef1cf46
JoeCodeswell <joecodeswell@gmail.com>
2019-10-28 12:31:08 -0700

    1 file modified, 2 added "card-joe"

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,10 +14,11 @@ import {
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { CardJoeComponent } from './card-joe/card-joe.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CardJoeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/card-joe/card-joe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-joe',
+  templateUrl: './card-joe.component.html',
+  styleUrls: ['./card-joe.component.css']
+})
+export class CardJoeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
